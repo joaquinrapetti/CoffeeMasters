d0121bf4fb664fee2cbcb441e7e8f9cc8fcb8dfe
joaquinrapetti <j.rapetti92@gmail.com>
2022-09-21 13:54:49 -0300

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Joaquin Rapetti Pozzoli on 21/9/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
