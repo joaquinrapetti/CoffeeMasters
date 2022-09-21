4c2e574d0ddcc60dd53edc2a1f374f67468ea379
joaquinrapetti <j.rapetti92@gmail.com>
2022-09-21 15:16:35 -0300

    color switches ready

--- CoffeeMasters/ContentView.swift
+++ CoffeeMasters/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // while i have nothing
+        EmptyView()
     }
 }
 
@@ -19,3 +19,53 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+// ----------------------- START  EXERCISE ----------------------------
+
+//struct ContentView: View {
+//    var body: some View {
+//        // Think a View as a Component, we can use reusable View
+//        // I can only return View (Fragment <></>)
+//        ScrollView {
+//            VStack {
+//                Text("Hola Mundo!")
+//                Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+//                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+//                }
+//                // Properties are Modifiers
+//                // Modifiers takes elements and modify to another elements
+//                ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+//                    Text("Hello World!")
+//                        // Text.modifiers
+//                        .font(.largeTitle)
+//                        .foregroundColor(.purple)
+//                        .padding(30)
+//                        .background(Color.yellow)
+//                        .padding(30)
+//                }
+//            }
+//        }
+//    }
+//}
+
+// ----------------------- COMPONENTS EXEIRCISE ------------------
+
+//struct ContentView: View {
+//    var body: some View {
+//        // Invoke greeting component into principal Component
+//        Greeting()
+//    }
+//}
+
+//// Greeting Component
+//struct Greeting: View {
+//    // @State, declare variable as an state container admit changes - state variable
+//    @State var name = ""
+//
+//    var body: some View {
+//        VStack {
+//            TextField("Enter your name", text: $name) // $ sign need to bind
+//            Text("Hello \(name)")
+//        }
+//    }
+//}
